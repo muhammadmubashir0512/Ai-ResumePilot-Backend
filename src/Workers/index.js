65007a003b8841057282e3d117a3ad6fd6febc57
Muhammad Mubashir <zayansaeed111@gmail.com>
2026-08-31 11:51:00 +0500

    refactor: remove worker initialization logic from index.js

--- src/Workers/index.js
+++ src/Workers/index.js
@@ -1,13 +1,2 @@
 import "dotenv/config";
 import connectDB from "../db/index.js";
-
-const startWorkers = async () => {
-  await connectDB();
-
-  await import("./email.worker.js");
-  await import("./resume.worker.js");
-
-  console.log("All workers started...");
-};
-
-startWorkers();
